51ebedba2387fed552bb55cdb8b1701836bd30ce
Karl Vincent C. Suayan <kvsuayan@gbox.ncf.edu.ph>
2024-11-11 21:35:38 +0800

    Update method.js

--- method.js
+++ method.js
@@ -5,11 +5,11 @@ Win Lose Draw Conditions
 */ 
 var rand1 = Math.floor(Math.random() * 6)+1;
 var randDice = "dice" + rand1 + ".png";
-var randSource = "../images/" + randDice;
+var randSource = "./images/" + randDice;
 document.querySelectorAll("img")[0].setAttribute("src", randSource);
 var rand2 = Math.floor(Math.random() * 6)+1;
 var randDice2 = "dice" + rand2 + ".png";
-var randSource2 = "../images/" + randDice2;
+var randSource2 = "./images/" + randDice2;
 document.querySelectorAll("img")[1].setAttribute("src", randSource2);
 if (rand1 > rand2) {  
   document.querySelector("h1").innerHTML = "Player One Wins!";
